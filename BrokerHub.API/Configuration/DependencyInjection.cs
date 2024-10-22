24cc011127e223fabdac24be29ce7bd39582c6c9
Mateus Mourao <mateusvmourao@gmail.com>
2024-10-22 00:58:00 -0300

    feat: adicionando conexão com o banco e serviços da aplicação

---
+++ BrokerHub.API/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using BrokerHub.Application.Command.Imovel.CreateImovel;
+using System.Reflection;
+
+namespace BrokerHub.API.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(CreateImovelHandler).Assembly);
+        });
+
+        return services;
+    }
+}
--- BrokerHub.API/Program.cs
+++ BrokerHub.API/Program.cs
@@ -1,25 +1,49 @@
+using System.Reflection;
+using BrokerHub.API.Configuration;
+using BrokerHub.Domain.Interfaces;
+using BrokerHub.Infrastructure.Persistence.Data;
+using BrokerHub.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddApplicationServices();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "BrokerHub API",
+        Version = "v1",
+        Description = "API para corretoras de imóveis: Cadastre e altere imóveis, consulte e agende visitas.",
+        Contact = new OpenApiContact()
+        {
+            Name = "Mateus Mourão",
+            Email = "mateusvmourao@gmail.com",
+            Url = new Uri("https://mathieux-dev.github.io/")
+        }
+    });
+    var arquivoSwaggerXML = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var diretorioArquivoXML = Path.Combine(AppContext.BaseDirectory, arquivoSwaggerXML);
+    c.IncludeXmlComments(diretorioArquivoXML);
+});
+
+builder.Services.AddDbContext<BrokerHubDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IImovelRepository, ImovelRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
+app.UseHttpsRedirection();
 app.MapControllers();
-
 app.Run();
